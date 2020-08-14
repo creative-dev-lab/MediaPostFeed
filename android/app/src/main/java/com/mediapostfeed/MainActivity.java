57dea913a0d53f9c0fb116cfff21c6c98bd81b39
Eli Zhang <elizhang221@gmail.com>
2020-08-14 13:19:14 -0700

    first commit

---
+++ android/app/src/main/java/com/mediapostfeed/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mediapostfeed;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "MediaPostFeed";
+  }
+}
