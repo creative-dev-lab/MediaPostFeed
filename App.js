e3843226c954bd26a270af4835aa3e91d1b898e3
Eli Zhang <elizhang221@gmail.com>
2020-08-14 14:22:42 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
+/**
+ * @author Eli Zhang
+ * created on 8/14/2020
+ * modified on 8/14/2020
+ */
+
 import * as React from 'react';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
